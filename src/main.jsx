eeb516f97af0afca2a2967a28afacbf208a0c1f5
liang799 <71926348+liang799@users.noreply.github.com>
2022-11-22 10:14:47 +0800

    Added 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage/>,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
